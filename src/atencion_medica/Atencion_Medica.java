a66f259d02232a7787e0e63cb409783e38e8d846
donnovan2023 <148404433+donnovan2023@users.noreply.github.com>
2023-11-30 00:59:54 -0300

    Add files via upload

---
+++ src/atencion_medica/Atencion_Medica.java
@@ -0,0 +1,17 @@
+
+package atencion_medica;
+
+import Interfaz.Seleccion;
+
+
+public class Atencion_Medica {
+
+
+    public static void main(String args[]) {
+        // Crear una instancia de la clase Seleccion
+        Seleccion seleccionFrame = new Seleccion();
+
+        // Hacer visible la instancia de Seleccion
+        seleccionFrame.setVisible(true);
+    }
+}
