db0c24a204f6b3cdb4d1439ef8ad9ea5de2528b1
donnovan2023 <148404433+donnovan2023@users.noreply.github.com>
2023-11-29 23:37:28 -0300

    Add files via upload
    Final

---
+++ src/Ingreso_interfaz1.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author donom
+ */
+public class Ingreso_interfaz1 {
+String   Nomb_ed;
+String   Nom_ac;
+String   Especialidad_ed;
+String   Horas_dis;
+int HorasMedicas;
+    public Ingreso_interfaz1(String Nomb_ed, String Nom_ac, String Especialidad_ed, String Horas_dis, int HorasMedicas) {
+        this.Nomb_ed = Nomb_ed;
+        this.Nom_ac = Nom_ac;
+        this.Especialidad_ed = Especialidad_ed;
+        this.Horas_dis = Horas_dis;
+        this.HorasMedicas = HorasMedicas;
+    }
+
+    public String getNomb_ed() {
+        return Nomb_ed;
+    }
+
+    public void setNomb_ed(String Nomb_ed) {
+        this.Nomb_ed = Nomb_ed;
+    }
+
+    public String getNom_ac() {
+        return Nom_ac;
+    }
+
+    public void setNom_ac(String Nom_ac) {
+        this.Nom_ac = Nom_ac;
+    }
+
+    public String getEspecialidad_ed() {
+        return Especialidad_ed;
+    }
+
+    public void setEspecialidad_ed(String Especialidad_ed) {
+        this.Especialidad_ed = Especialidad_ed;
+    }
+
+    public String getHoras_dis() {
+        return Horas_dis;
+    }
+
+    public void setHoras_dis(String Horas_dis) {
+        this.Horas_dis = Horas_dis;
+    }
+
+    public int getHorasMedicas() {
+        return HorasMedicas;
+    }
+
+    public void setHorasMedicas(int HorasMedicas) {
+        this.HorasMedicas = HorasMedicas;
+    }
+
+
+
+
+
+
+}
